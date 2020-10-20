bb5225e788524928aee9767d1b8faeefbc89fbdf
Tomasz Gałkowski <galkowski.t@gmail.com>
2020-10-21 01:58:55 +0200

    adds syntax coloring for json responses

--- cmd/main.go
+++ cmd/main.go
@@ -1,64 +1,71 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http/httputil"
-	"os"
+    "bytes"
+    "encoding/json"
+    "fmt"
+    "io/ioutil"
+    "log"
+    "net/http/httputil"
+    "os"
 
-	"github.com/galkowskit/zvrotka/http"
-	"github.com/galkowskit/zvrotka/parser"
+    "github.com/alecthomas/chroma/quick"
+
+    "github.com/galkowskit/zvrotka/http"
+    "github.com/galkowskit/zvrotka/parser"
 )
 
 func main() {
-	path := os.Args[1]
-	name := os.Args[2]
+    path := os.Args[1]
+    name := os.Args[2]
 
-	file, err := os.Open(path)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
+    file, err := os.Open(path)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer file.Close()
 
-	reqs, err := parser.ReadRequests(file)
-	if err != nil {
-		log.Fatal(err)
-	}
+    reqs, err := parser.ReadRequests(file)
+    if err != nil {
+        log.Fatal(err)
+    }
 
-	var req http.Request
-	for _, r := range reqs {
-		if r.Name == name {
-			req = r
-			break
-		}
-	}
+    var req http.Request
+    for _, r := range reqs {
+        if r.Name == name {
+            req = r
+            break
+        }
+    }
 
-	res, err := http.SendRequest(req)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer res.Res.Body.Close()
+    res, err := http.SendRequest(req)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer res.Res.Body.Close()
 
-	data, err := ioutil.ReadAll(res.Res.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
+    data, err := ioutil.ReadAll(res.Res.Body)
+    if err != nil {
+        log.Fatal(err)
+    }
 
-	dump, err := httputil.DumpResponse(res.Res, true)
-	if err != nil {
-		log.Fatal(err)
-	}
+    dump, err := httputil.DumpResponse(res.Res, true)
+    if err != nil {
+        log.Fatal(err)
+    }
 
-	var prettyJSON bytes.Buffer
-	err = json.Indent(&prettyJSON, data, "", "\t")
-	if err != nil {
-		log.Fatal(err)
-	}
+    var prettyJSON bytes.Buffer
+    err = json.Indent(&prettyJSON, data, "", "\t")
+    if err != nil {
+        log.Fatal(err)
+    }
 
-	fmt.Println(string(dump))
-	fmt.Printf("Duration: %vms\n\n", res.Duration.Milliseconds())
-	fmt.Println(string(prettyJSON.Bytes()))
+    indented := string(prettyJSON.Bytes())
+
+    fmt.Println(string(dump))
+    fmt.Printf("Duration: %vms\n\n", res.Duration.Milliseconds())
+    err = quick.Highlight(os.Stdout, indented, "json", "terminal16m", "dracula")
+    if err != nil {
+        log.Fatal(err)
+    }
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/galkowskit/zvrotka
 
 go 1.15
 
-require github.com/serverhorror/rog-go v0.0.0-20150622204439-87e4e0161534 // indirect
+require (
+	github.com/alecthomas/chroma v0.8.1 // indirect
+	github.com/serverhorror/rog-go v0.0.0-20150622204439-87e4e0161534 // indirect
+)
