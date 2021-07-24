6ebcee9681d49b7ffc10aaebe9de825cd5aa7ded
Tomasz Gałkowski <galkowski.t@gmail.com>
2021-07-24 17:32:14 +0200

    basic cobra and promptui implementation (#4)

--- cmd/main.go
+++ cmd/main.go
@@ -1,71 +1,9 @@
 package main
 
 import (
-    "bytes"
-    "encoding/json"
-    "fmt"
-    "io/ioutil"
-    "log"
-    "net/http/httputil"
-    "os"
-
-    "github.com/alecthomas/chroma/quick"
-
-    "github.com/galkowskit/zvrotka/http"
-    "github.com/galkowskit/zvrotka/parser"
+	"github.com/galkowskit/zvrotka/cmd/zvrotka"
 )
 
 func main() {
-    path := os.Args[1]
-    name := os.Args[2]
-
-    file, err := os.Open(path)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer file.Close()
-
-    reqs, err := parser.ReadRequests(file)
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    var req http.Request
-    for _, r := range reqs {
-        if r.Name == name {
-            req = r
-            break
-        }
-    }
-
-    res, err := http.SendRequest(req)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer res.Res.Body.Close()
-
-    data, err := ioutil.ReadAll(res.Res.Body)
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    dump, err := httputil.DumpResponse(res.Res, true)
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    var prettyJSON bytes.Buffer
-    err = json.Indent(&prettyJSON, data, "", "\t")
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    indented := string(prettyJSON.Bytes())
-
-    fmt.Println(string(dump))
-    fmt.Printf("Duration: %vms\n\n", res.Duration.Milliseconds())
-    err = quick.Highlight(os.Stdout, indented, "json", "terminal16m", "dracula")
-    if err != nil {
-        log.Fatal(err)
-    }
+	zvrotka.Execute()
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/galkowskit/zvrotka
 go 1.15
 
 require (
-	github.com/alecthomas/chroma v0.8.1 // indirect
+	github.com/alecthomas/chroma v0.8.1
+	github.com/manifoldco/promptui v0.8.0
 	github.com/serverhorror/rog-go v0.0.0-20150622204439-87e4e0161534 // indirect
+	github.com/spf13/cobra v1.2.1
 )
